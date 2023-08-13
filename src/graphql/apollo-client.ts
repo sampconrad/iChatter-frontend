d8ef4ab6c94c298434d5b94ea9c05016d3d70ba7
Conrado Sampaio <102433904+sampconrad@users.noreply.github.com>
2023-08-13 14:09:05 -0300

    updated apollo links

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -4,11 +4,14 @@ import { getMainDefinition } from "@apollo/client/utilities";
 import { createClient } from "graphql-ws";
 import { getSession } from "next-auth/react";
 
+const APOLLO_WSLINK = "ws://ichatter-backend-production.up.railway.app/graphql/subscriptions";
+const APOLLO_HTTPLINK = "http://ichatter-backend-production.up.railway.app/graphql"
+
 const wsLink =
   typeof window !== "undefined"
     ? new GraphQLWsLink(
         createClient({
-          url: `${process.env.APOLLO_WSLINK}`,
+          url: APOLLO_WSLINK,
           connectionParams: async () => ({
             session: await getSession(),
           }),
@@ -17,7 +20,7 @@ const wsLink =
     : null;
 
 const httpLink = new HttpLink({
-  uri: `${process.env.APOLLO_HTTPLINK}`,
+  uri: APOLLO_HTTPLINK,
   credentials: "include",
 });
 
